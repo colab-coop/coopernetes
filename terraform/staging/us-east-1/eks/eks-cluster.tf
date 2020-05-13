9679613d6e6f6bee7146a9d9d2ed0d61faa500e5
Ian Davis <ianconsolata@gmail.com>
2020-05-13 10:24:49 -0700

    Increase capacity of cluster

--- terraform/staging/us-east-1/eks/eks-cluster.tf
+++ terraform/staging/us-east-1/eks/eks-cluster.tf
@@ -10,13 +10,13 @@ module "eks" {
   vpc_id = module.vpc.vpc_id
 
   worker_groups = [
-    #{
-    #  name                          = "worker-group-1"
-    #  instance_type                 = "t2.small"
-    #  additional_userdata           = "echo foo bar"
-    #  asg_desired_capacity          = 2
-    #  additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-    #},
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t2.small"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+    },
     {
       name                          = "worker-group-2"
       instance_type                 = "t2.medium"
