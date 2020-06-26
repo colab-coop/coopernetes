5f002dfde333e82b7c5ceacaac32540def9c0262
Ian Davis <ianconsolata@gmail.com>
2020-06-26 15:43:14 -0700

    No longer creating ECR repos, just using dockerhub

--- terraform/staging/us-east-1/apps/agendr.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "agendr" {
-  name                 = "agendr"
-  image_tag_mutability = "IMMUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
