69825c2178a753cfdd9f588a2f7d04888010f46e
Ian Davis <ianconsolata@gmail.com>
2020-07-13 18:21:37 -0700

    Move the terraform folder, and use tfvars files to configure clusters.
    This makes the terraform folder totally cluster agnostic, and lets us
    keep one .tfvars file per cluster.

--- terraform/eks/eks-cluster.tf
+++ terraform/eks/eks-cluster.tf
@@ -34,17 +34,17 @@ module "eks" {
   worker_groups = [
     {
       name                          = "worker-group-1"
-      instance_type                 = "t2.small"
+      instance_type                 = var.worker_group_1_type
       additional_userdata           = "echo foo bar"
-      asg_desired_capacity          = 2
+      asg_desired_capacity          = var.worker_group_1_count
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
     {
       name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
+      instance_type                 = var.worker_group_2_type
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
+      asg_desired_capacity          = var.worker_group_2_count
     },
   ]
 }
--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -7,16 +7,22 @@ variable "env" {
   default = "test"
 }
 
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
+resource "random_pet" "suffix" {}
 
 variable "cluster_name" {
   default = "UNDEFINED"
 }
 
+variable "worker_group_1" {
+  default = {}
+}
+
+variable "worker_group_1_type" {}
+variable "worker_group_1_count" {}
+variable "worker_group_2_type" {}
+variable "worker_group_2_count" {}
+
 locals {
-  default_cluster_name = "coopernetes-${var.env}-${random_string.suffix.result}"
+  default_cluster_name = "coopernetes-${var.env}-${random_pet.suffix.id}"
   cluster_name         = "${var.cluster_name == "UNDEFINED" ? local.default_cluster_name : var.cluster_name}"
 }
