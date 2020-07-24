6185ac7dbc29d26d4812e65418ae55721bc614d7
Ian Davis <ianconsolata@gmail.com>
2020-07-24 12:20:36 -0700

    Make both deploy scripts tag releases
    Also deploy things one a time so we don't create deploys when things
    don't change.

--- charts/package-and-deploy.sh
+++ charts/package-and-deploy.sh
@@ -1,6 +1,18 @@
 #! /bin/sh
 
+set -ex
+
+
+dir=$1
 repo=$(git rev-parse --show-toplevel)/docs/charts/
-helm package */
-mv *.tgz $repo
+
+helm package $dir
+chart=$(find . -name "$dir-*.tgz" | sed "s|^\./||")
+mv $chart $repo
 helm repo index $repo --url https://colab-coop.github.io/coopernetes/charts/
+
+git_tag=${chart%.tgz}
+git add -A $repo
+git commit -m "Release $git_tag"
+git tag $git_tag
+git push --tags
--- images/build-and-push.sh
+++ images/build-and-push.sh
@@ -2,15 +2,17 @@
 
 set -ex
 
-for dir in */; do
-  cd $dir
-  image=${dir%/}
-  version=`cat VERSION`
-  image_tag="colabcoop/$image:$version"
-  git_tag="image-$image-$version"
-  git tag $git_tag
-  git push --tags
-  docker build -t $image_tag .
-  docker push $image_tag
-  cd ..
-done
+dir=$1
+cd $dir
+version=`cat VERSION`
+
+image=${dir%/}
+image_tag="colabcoop/$image:$version"
+docker build -t $image_tag .
+docker push $image_tag
+
+git_tag="image-$image-$version"
+git tag $git_tag
+git push --tags
+
+cd ..
